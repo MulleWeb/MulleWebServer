e19f1c52e704d4f76f7bda467f5a9b1e50fbaac3
Nat! <nat@mulle-kybernetik.com>
2024-11-19 15:33:12 +0100

    update various version files

--- test/.mulle/share/env/environment-extension.sh
+++ test/.mulle/share/env/environment-extension.sh
@@ -29,13 +29,15 @@ export MULLE_MATCH_FIND_NAMES="*.environment,*.args,*.stdout,*.stdin,*.stderr,*.
 
 
 #
-# Make project to test discoverable via MULLE_FETCH_SEARCH_PATH$'\n'We assume we are in ${PROJECT_DIR}/test so modify search path to add ../..
+# Make project to test discoverable via MULLE_FETCH_SEARCH_PATH
+# We assume we are in ${PROJECT_DIR}/test so modify search path to add ../..
 #
 export MULLE_FETCH_SEARCH_PATH="${MULLE_FETCH_SEARCH_PATH}:${MULLE_VIRTUAL_ROOT}/../.."
 
 
 #
-# For more aggressive leak testing, it is good if singletons cleanup$'\n'in the NSAutoreleasePool. These ephemerals aren't thread safe singletons.
+# For more aggressive leak testing, it is good if singletons cleanup
+# in the NSAutoreleasePool. These ephemerals aren't thread safe singletons.
 #
 export MULLE_OBJC_EPHEMERAL_SINGLETON="YES"
 
--- test/.mulle/share/env/environment-post-extension.sh
+++ test/.mulle/share/env/environment-post-extension.sh
@@ -1,5 +1,6 @@
 #
-# Used to be hardcoded in mulle-sde plugin, now part of the environment.$'\n'pick first as default (if any)
+# Used to be hardcoded in mulle-sde plugin, now part of the environment.
+# pick first as default (if any)
 #
 export _MULLE_SDK_DIR="${DEPENDENCY_DIR:-${MULLE_VIRTUAL_ROOT}/dependency}/${MULLE_CRAFT_SDKS%%:*}"
 
